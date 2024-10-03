d37e6bc3bda28efb000b672150be0547c02e106d
poseidon2022 <kidusm82@gmail.com>
2024-10-03 10:18:52 +0300

    controller and usecase done

---
+++ api/controller/controller.js
@@ -0,0 +1,124 @@
+class PersonController {
+    constructor(personUseCase) {
+        this.personUseCase = personUseCase
+    }
+
+    async Register(req, res) {
+        try {
+            const {name, age, hobbies} = req.body
+            if (typeof name != String ||
+                name.trim() == '' || typeof age != Number
+                || age < 0 || typeof hobbies != Array) {
+                    res.status(400).json({
+                        success : false,
+                        message : "invalid request format"
+                    })
+            }
+
+            const registeredPerson = await this.personUseCase.Register(name, age, hobbies)
+            res.status(200).json({
+                success : true, 
+                message : "person registered successfully",
+                person : registeredPerson
+            })
+        } catch(err) {
+            res.status(500).json({
+                success : false, 
+                message : "internal server error"
+            })
+        }
+    }
+
+    async UpdatePerson(req, res) {
+        const personID = req.params("person_id")
+        try {
+            const {name, age, hobbies} = req.body
+            if (name && (typeof name != String ||
+                name.trim() == '') || (age && typeof age != Number
+                || age < 0) || (hobbies && typeof hobbies != Array)) {
+                    res.status(400).json({
+                        success : false,
+                        message : "invalid request format"
+                    })
+            }
+
+            const updatedPerson = await this.personUseCase.UpdatePerson(personID, name, age, hobbies)
+            res.status(200).json({
+                success : true,
+                message : "person info updated successfully",
+                updated_person : updatedPerson
+            })
+        } catch(err) {
+            res.status(500).json({
+                success : false,
+                message : "internal server error"
+            })
+        }
+    }
+
+    async DeletePerson(req, res ){
+        const personiD = req.params("person_id")
+        try {
+            const deletedPerson = await this.personUseCase.DeletePerson(personiD)
+            res.status(200).json({
+                success : false, 
+                message : "person deleted successfully",
+                deleted_person : deletedPerson
+            })
+        } catch(err) {
+            if (err.message == "person with the specified id not found") {
+                res.status(404).json({
+                    success : false, 
+                    message : "person with the specified id not found"
+                })
+                return
+            }
+            res.status(500).json({
+                success : false,
+                message : "internal server error"
+            })
+        } 
+    }
+
+    async GetPersonByID(req, res ){
+        const personiD = req.params("person_id")
+        try {
+            const foundPerson = await this.personUseCase.GetPersonByID(personiD)
+            res.status(200).json({
+                success : false, 
+                message : "person fetched successfully",
+                deleted_person : foundPerson
+            })
+        } catch(err) {
+            if (err.message == "person with the specified id not found") {
+                res.status(404).json({
+                    success : false, 
+                    message : "person with the specified id not found"
+                })
+                return
+            }
+            res.status(500).json({
+                success : false,
+                message : "internal server error"
+            })
+        } 
+    }
+
+    async GetAllPersons(req, res) {
+        try {
+            allPersons = await this.personUseCase.GetAllPersons()
+            res.status(500).json({
+                success : true,
+                message : "persons fetched successfully",
+                persons : allPersons
+            })
+        } catch(err) {
+            res.status(500).json({
+                success : false,
+                message : "internal server error"
+            })
+        }
+    }
+}
+
+module.exports = PersonController
---
+++ usecase/usecase.js
@@ -0,0 +1,50 @@
+class PersonUseCase {
+    constructor(personRepository) {
+        this.personRepository = personRepository
+    }
+
+    async Register(name, age, hobbies) {
+        try {
+            const registeredPerson = await this.personRepository.Register(name, age, hobbies)
+            return registeredPerson
+        } catch(error) {
+            throw new Error(error.message)
+        }
+    }
+
+    async UpdatePerson(personID, name, age, hobbies) {
+        try {
+            const updatedPerson = await this.personRepository.UpdatePerson(personID, name, age, hobbies)
+            return updatedPerson
+        } catch(error) {
+            throw new Error(error.message)
+        }
+    }
+
+    async DeletePerson(personID) {
+        try {
+            const deletedPerson = await this.personRepository.DeletePerson(personID)
+            return deletedPerson
+        } catch(error) {
+            throw new Error(error.message)
+        }
+    }
+
+    async GetPersonByID(personID) {
+        try {
+            const foundPerson = await this.personRepository.GetPersonByID(personID)
+            return foundPerson
+        } catch(error) {
+            throw new Error(error.message)
+        }
+    }
+
+    async GetAllPersons() {
+        try {
+            const allPersons = await this.personRepository.GetAllPersons()
+            return allPersons
+        } catch(error) {
+            throw new Error(error.message)
+        }
+    }
+}
